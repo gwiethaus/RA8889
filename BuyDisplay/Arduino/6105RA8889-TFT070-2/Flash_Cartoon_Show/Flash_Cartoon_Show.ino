47ea6c49b23005d2b64c0da948c31b864d64202c
Guilherme Wiethaus <58825334+gwiethaus@users.noreply.github.com>
2025-08-26 22:04:44 -0300

    Add files via upload

---
+++ BuyDisplay/Arduino/6105RA8889-TFT070-2/Flash_Cartoon_Show/Flash_Cartoon_Show.ino
@@ -0,0 +1,151 @@
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+Examples for ER-PCBA6105RA8889+ER-ER-TFT070-2  Flash_Cartoon_Show
+Display is Hardward SPI 4-Wire SPI Interface and 5V Power Supply
+Tested and worked with:
+Works with Arduino 1.6.0 IDE  
+Test ok:  Arduino Due,Arduino UNO,Arduino MEGA2560
+****************************************************/
+
+#include <SPI.h>
+#include <Wire.h>
+#include "RA8889.h"
+
+void setup() {
+  
+  pinMode(5,   OUTPUT);
+  digitalWrite(5, HIGH);//Disable  SD 
+  pinMode(2,   OUTPUT);
+  digitalWrite(2, HIGH);//Disable  RTP    
+  
+  ER5517.Parallel_Init();
+  ER5517.HW_Reset();
+  ER5517.System_Check_Temp();
+  delay(100);
+  while(ER5517.LCD_StatusRead()&0x02);
+  ER5517.initial();
+  ER5517.Display_ON();
+
+}
+
+
+void LCD_BTE_Memory_Copy
+(
+ unsigned long S0_Addr     
+,unsigned short S0_W       
+,unsigned short XS0        
+,unsigned short YS0      
+,unsigned long S1_Addr    
+,unsigned short S1_W       
+,unsigned short XS1        
+,unsigned short YS1        
+,unsigned long Des_Addr    
+,unsigned short Des_W     
+,unsigned short XDes      
+,unsigned short YDes    
+,unsigned int ROP_Code    
+/*ROP_Code :
+   0000b		0(Blackness)
+   0001b		~S0!E~S1 or ~(S0+S1)
+   0010b		~S0!ES1
+   0011b		~S0
+   0100b		S0!E~S1
+   0101b		~S1
+   0110b		S0^S1
+   0111b		~S0 + ~S1 or ~(S0 + S1)
+   1000b		S0!ES1
+   1001b		~(S0^S1)
+   1010b		S1
+   1011b		~S0+S1
+   1100b		S0
+   1101b		S0+~S1
+   1110b		S0+S1
+   1111b		1(whiteness)*/
+,unsigned short X_W      
+,unsigned short Y_H       
+)
+{
+  ER5517.BTE_S0_Color_16bpp();
+  ER5517.BTE_S0_Memory_Start_Address(S0_Addr);
+  ER5517.BTE_S0_Image_Width(S0_W);
+  ER5517.BTE_S0_Window_Start_XY(XS0,YS0);
+
+  ER5517.BTE_S1_Color_16bpp();
+  ER5517.BTE_S1_Memory_Start_Address(S1_Addr);
+  ER5517.BTE_S1_Image_Width(S1_W); 
+  ER5517.BTE_S1_Window_Start_XY(XS1,YS1);
+
+  ER5517.BTE_Destination_Color_16bpp();
+  ER5517.BTE_Destination_Memory_Start_Address(Des_Addr);
+  ER5517.BTE_Destination_Image_Width(Des_W);
+  ER5517.BTE_Destination_Window_Start_XY(XDes,YDes);	
+   
+  ER5517.BTE_ROP_Code(ROP_Code);	
+  ER5517.BTE_Operation_Code(0x02); //BTE Operation: Memory copy (move) with ROP.
+  ER5517.BTE_Window_Size(X_W,Y_H); 
+  ER5517.BTE_Enable();
+  ER5517.Check_BTE_Busy();
+}
+
+
+
+
+
+void loop() {
+
+#define Cartoon_Addr      3840000
+
+#define Picture_1_Addr	  0
+#define Picture_2_Addr	  768000 //(LCD_XSIZE_TFT*LCD_YSIZE_TFT*2)
+#define Picture_3_Addr	  1536000//(LCD_XSIZE_TFT*LCD_YSIZE_TFT*4)
+#define Picture_4_Addr	  2304000 //(LCD_XSIZE_TFT*LCD_YSIZE_TFT*6)
+#define Picture_5_Addr	  3072000 //(LCD_XSIZE_TFT*LCD_YSIZE_TFT*8)
+
+   ER5517.Select_Main_Window_16bpp();
+  ER5517.Main_Image_Start_Address(layer2_start_addr);        
+  ER5517.Main_Image_Width(LCD_XSIZE_TFT);
+  ER5517.Main_Window_Start_XY(0,0);
+  ER5517.Canvas_Image_Start_address(layer2_start_addr);
+  ER5517.Canvas_image_width(LCD_XSIZE_TFT);
+  ER5517.Active_Window_XY(0,0);
+  ER5517.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT); 
+  ER5517.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Blue);
+  ER5517.DMA_24bit_Block(1,1,0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,LCD_XSIZE_TFT,Picture_3_Addr);
+	
+  ER5517.Canvas_image_width(223);
+  ER5517.Canvas_Image_Start_address(layer3_start_addr);
+  ER5517.DMA_24bit_Block(1,0,0,0,223,134*30,223,Cartoon_Addr); 
+  
+	
+       unsigned char i;
+	unsigned char temp ;
+	while(1)
+	{temp = 30;
+		for(i = 0 ; i < 30 ; i++)
+		{
+			LCD_BTE_Memory_Copy(layer3_start_addr,223,0,134*i,
+							layer3_start_addr,223,0,134*i,
+							layer2_start_addr,LCD_XSIZE_TFT,88,53,0X0C,220,134);
+			
+			LCD_BTE_Memory_Copy(layer3_start_addr,223,0,134*i,
+							layer3_start_addr,223,0,134*i,
+							layer2_start_addr,LCD_XSIZE_TFT,88,293,0X0C,220,134);
+			
+			
+				temp = temp - 1;
+			
+			LCD_BTE_Memory_Copy(layer3_start_addr,223,0,134*temp,
+							layer3_start_addr,223,0,134*temp,
+							layer2_start_addr,LCD_XSIZE_TFT,488,53,0X0C,220,134);
+			
+			LCD_BTE_Memory_Copy(layer3_start_addr,223,0,134*temp,
+							layer3_start_addr,223,0,134*temp,
+							layer2_start_addr,LCD_XSIZE_TFT,488,293,0X0C,220,134);
+		  delay(50);
+		
+		}
+	}					   
+  
+    
+}
---
+++ BuyDisplay/Arduino/6105RA8889-TFT070-2/SD_test/SD_test.ino
@@ -0,0 +1,252 @@
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+Examples for ER-PCBA6105RA8889+ER-TFT070-2  SDcard(bitmap) test
+Display is Hardward SPI 4-Wire SPI Interface and 5V Power Supply
+Tested and worked with:
+Works with Arduino 1.6.0 IDE  
+Test ok:  Arduino Due,Arduino UNO,Arduino MEGA2560
+****************************************************/
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "RA8889.h"
+
+#define sd_cs 5   
+
+void setup() {
+  Serial.begin(9600);
+  if (!SD.begin(sd_cs)) 
+  {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done."); 
+   
+  ER5517.Parallel_Init();
+  ER5517.HW_Reset();
+  ER5517.System_Check_Temp();
+  delay(100);
+  while(ER5517.LCD_StatusRead()&0x02);
+  ER5517.initial();
+  ER5517.Display_ON();
+     pinMode(2,   OUTPUT);
+    digitalWrite(2, HIGH);//Disable  RTP
+}
+void loop() {
+  ER5517.Select_Main_Window_16bpp();
+  ER5517.Main_Image_Start_Address(layer1_start_addr);        
+  ER5517.Main_Image_Width(LCD_XSIZE_TFT);
+  ER5517.Main_Window_Start_XY(0,0);
+  ER5517.Canvas_Image_Start_address(0);
+  ER5517.Canvas_image_width(LCD_XSIZE_TFT);
+  ER5517.Active_Window_XY(0,0);
+  ER5517.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT); 
+  
+  ER5517.Foreground_color_65k(Blue);
+  ER5517.Line_Start_XY(0,0);
+  ER5517.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER5517.Start_Square_Fill(); 
+  
+   ER5517.Foreground_color_65k(White);
+  ER5517.Background_color_65k(Red);
+  ER5517.CGROM_Select_Internal_CGROM();  
+  ER5517.Font_Select_12x24_24x24();
+  ER5517.Goto_Text_XY(0,0); 
+  ER5517.Show_String( "www.buydisplay.com"); 
+  
+    bmpDraw("BMP_1.bmp", 0, 0);
+   delay(1000);
+   bmpDraw("BMP_2.bmp", 0, 0); 
+  delay(1000);
+   bmpDraw("BMP_3.bmp", 0, 0); 
+  delay(1000);
+   bmpDraw("BMP_4.bmp", 0, 0);    
+    delay(1000);
+   bmpDraw("BMP_5.bmp", 0, 0);   
+     delay(1000);
+   bmpDraw("BMP_6.bmp", 0, 0); 
+    delay(1000);
+   bmpDraw("BMP_7.bmp", 0, 0);   
+     delay(1000);
+   bmpDraw("BMP_8.bmp", 0, 0);  
+     delay(1000);
+   bmpDraw("BMP_9.bmp", 0, 0);  
+     delay(1000);
+   bmpDraw("BMP_10.bmp", 0, 0);  
+     delay(1000);   
+}
+
+
+// This function opens a Windows Bitmap (BMP) file and
+// displays it at the given coordinates.  It's sped up
+// by reading many pixels worth of data at a time
+// (rather than pixel by pixel).  Increasing the buffer
+// size takes more of the Arduino's precious RAM but
+// makes loading a little faster.  20 pixels seems a
+// good balance.
+
+#define BUFFPIXEL 20
+
+void bmpDraw(char *filename, int x, int y) {
+  File     bmpFile;
+  int      bmpWidth, bmpHeight;   // W+H in pixels
+  uint8_t  bmpDepth;              // Bit depth (currently must be 24)
+  uint32_t bmpImageoffset;        // Start of image data in file
+  uint32_t rowSize;               // Not always = bmpWidth; may have padding
+  uint8_t  sdbuffer[3*BUFFPIXEL]; // pixel in buffer (R+G+B per pixel)
+  uint16_t lcdbuffer[BUFFPIXEL];  // pixel out buffer (16-bit per pixel)
+  uint8_t  buffidx = sizeof(sdbuffer); // Current position in sdbuffer
+  boolean  goodBmp = false;       // Set to true on valid header parse
+  boolean  flip    = true;        // BMP is stored bottom-to-top
+  int      w, h, row, col;
+  uint8_t  r, g, b;
+  uint32_t pos = 0, startTime = millis();
+  uint8_t  lcdidx = 0;
+  boolean  first = true;
+
+  if((x >= LCD_XSIZE_TFT) || (y >= LCD_YSIZE_TFT)) return;
+
+  Serial.println();
+  Serial.print(F("Loading image '"));
+  Serial.print(filename);
+  Serial.println('\'');
+
+  // Open requested file on SD card
+  if ((bmpFile = SD.open(filename)) == NULL) {
+    Serial.println(F("File not found"));
+    return;
+  }
+
+  // Parse BMP header
+  if(read16(bmpFile) == 0x4D42) { // BMP signature
+    Serial.println(F("File size: ")); 
+    Serial.println(read32(bmpFile));
+    (void)read32(bmpFile); // Read & ignore creator bytes
+    bmpImageoffset = read32(bmpFile); // Start of image data
+    Serial.print(F("Image Offset: ")); 
+    Serial.println(bmpImageoffset, DEC);
+
+    // Read DIB header
+    Serial.print(F("Header size: ")); 
+    Serial.println(read32(bmpFile));
+    bmpWidth  = read32(bmpFile);
+    bmpHeight = read32(bmpFile);
+
+    if(read16(bmpFile) == 1) { // # planes -- must be '1'
+      bmpDepth = read16(bmpFile); // bits per pixel
+      Serial.print(F("Bit Depth: ")); 
+      Serial.println(bmpDepth);
+      if((bmpDepth == 24) && (read32(bmpFile) == 0)) { // 0 = uncompressed
+        goodBmp = true; // Supported BMP format -- proceed!
+        Serial.print(F("Image size: "));
+        Serial.print(bmpWidth);
+        Serial.print('x');
+        Serial.println(bmpHeight);
+
+        // BMP rows are padded (if needed) to 4-byte boundary
+        rowSize = (bmpWidth * 3 + 3) & ~3;
+
+        // If bmpHeight is negative, image is in top-down order.
+        // This is not canon but has been observed in the wild.
+        if(bmpHeight < 0) {
+          bmpHeight = -bmpHeight;
+          flip      = false;
+        }
+
+        // Crop area to be loaded
+        w = bmpWidth;
+        h = bmpHeight;
+        if((x+w-1) >= LCD_XSIZE_TFT)  w = LCD_XSIZE_TFT  - x;
+        if((y+h-1) >= LCD_YSIZE_TFT) h = LCD_YSIZE_TFT - y;
+
+        // Set TFT address window to clipped image bounds
+
+        for (row=0; row<h; row++) { // For each scanline...
+          // Seek to start of scan line.  It might seem labor-
+          // intensive to be doing this on every line, but this
+          // method covers a lot of gritty details like cropping
+          // and scanline padding.  Also, the seek only takes
+          // place if the file position actually needs to change
+          // (avoids a lot of cluster math in SD library).
+          if(flip) // Bitmap is stored bottom-to-top order (normal BMP)
+            pos = bmpImageoffset + (bmpHeight - 1 - row) * rowSize;
+          else     // Bitmap is stored top-to-bottom
+          pos = bmpImageoffset + row * rowSize;
+          if(bmpFile.position() != pos) { // Need seek?
+            bmpFile.seek(pos);
+            buffidx = sizeof(sdbuffer); // Force buffer reload
+          }
+
+          for (col=0; col<w; col++) { // For each column...
+            // Time to read more pixel data?
+            if (buffidx >= sizeof(sdbuffer)) { // Indeed
+              // Push LCD buffer to the display first
+              if(lcdidx > 0) {
+                  ER5517.DrawPixel(col+x, row+y, lcdbuffer[lcdidx]);
+                lcdidx = 0;
+                first  = false;
+              }
+
+              bmpFile.read(sdbuffer, sizeof(sdbuffer));
+              buffidx = 0; // Set index to beginning
+            }
+
+            // Convert pixel from BMP to TFT format
+            b = sdbuffer[buffidx++];
+            g = sdbuffer[buffidx++];
+            r = sdbuffer[buffidx++];
+            lcdbuffer[lcdidx] = color565(r,g,b);
+              ER5517.DrawPixel(col+x, row+y, lcdbuffer[lcdidx]);
+          } // end pixel
+
+        } // end scanline
+
+        // Write any remaining data to LCD
+        if(lcdidx > 0) {
+            ER5517.DrawPixel(col, row, lcdbuffer[lcdidx]);
+        } 
+
+        Serial.print(F("Loaded in "));
+        Serial.print(millis() - startTime);
+        Serial.println(" ms");
+
+      } // end goodBmp
+    }
+  }
+
+  bmpFile.close();
+  if(!goodBmp) Serial.println(F("BMP format not recognized."));
+
+}
+
+// These read 16- and 32-bit types from the SD card file.
+// BMP data is stored little-endian, Arduino is little-endian too.
+// May need to reverse subscript order if porting elsewhere.
+
+uint16_t read16(File f) {
+  uint16_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read(); // MSB
+  return result;
+}
+
+uint32_t read32(File f) {
+  uint32_t result;
+  ((uint8_t *)&result)[0] = f.read(); // LSB
+  ((uint8_t *)&result)[1] = f.read();
+  ((uint8_t *)&result)[2] = f.read();
+  ((uint8_t *)&result)[3] = f.read(); // MSB
+  return result;
+}
+
+uint16_t color565(uint8_t r, uint8_t g, uint8_t b) {
+  return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
+}
+
+byte decToBcd(byte val){
+  // Convert normal decimal numbers to binary coded decimal
+  return ( (val/10*16) + (val%10) );
+}
+
+
