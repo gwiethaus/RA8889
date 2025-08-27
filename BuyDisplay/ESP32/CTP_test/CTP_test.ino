f38703ac7b52a2912cc8bda5a8759fbed8719bc5
Guilherme Wiethaus <58825334+gwiethaus@users.noreply.github.com>
2025-08-26 22:09:06 -0300

    Add files via upload

---
+++ BuyDisplay/ESP32/CTP_test/CTP_test.ino
@@ -0,0 +1,319 @@
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+Examples for ER-PCBA6105RA8889+ER-TFT070-2 Capacitive touch screen test
+Display is Hardward SPI 4-Wire SPI Interface and 5V Power Supply,CTP is I2C interface.
+Tested and worked with:
+Works with Arduino 2.3.4 IDE  
+NOTE: test OK:ESP32-WROOM-32 
+****************************************************/
+
+/*
+  ==TFT Hardware SPI to ESP32  ==
+    TFT   =>    ESP32
+  *1,2. GND       ->    GND
+  *3,4. VCC       ->    5V    3.3V OR 5V is optional, depending on the voltage of the module purchased 
+  *5. CS        ->       5
+  *6. MISO        ->    19  
+  *7. MOSI       ->    23  
+  *8. SCK       ->    18  
+  *11. RES       ->    16   
+  *33. CTPINT     ->    17
+  *34. CTPSDA    ->    21
+  *35. CTPSCL    ->    22 
+  *36. CTPRST     ->    15
+*/
+
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "RA8889.h"
+
+uint8_t addr  = 0x38;  //CTP IIC ADDRESS
+
+#define FT5316_RST 15
+#define FT5316_INT   17  
+
+
+
+enum {
+  eNORMAL = 0,
+  eTEST   = 0x04,
+  eSYSTEM = 0x01
+};
+
+
+struct TouchLocation
+{
+  uint16_t x;
+  uint16_t y;
+};
+
+TouchLocation touchLocations[5];
+
+uint8_t readFT5316TouchRegister( uint8_t reg );
+uint8_t readFT5316TouchLocation( TouchLocation * pLoc, uint8_t num );
+uint8_t readFT5316TouchAddr( uint8_t regAddr, uint8_t * pBuf, uint8_t len );
+
+uint32_t dist(const TouchLocation & loc);
+uint32_t dist(const TouchLocation & loc1, const TouchLocation & loc2);
+
+bool sameLoc( const TouchLocation & loc, const TouchLocation & loc2 );
+
+
+uint8_t buf[30];
+
+uint8_t readFT5316TouchRegister( uint8_t reg )
+{
+  Wire.beginTransmission(addr);
+  Wire.write( reg );  // register 0
+  uint8_t retVal = Wire.endTransmission();
+  
+  uint8_t returned = Wire.requestFrom(addr, uint8_t(1) );    // request 6 uint8_ts from slave device #2
+  
+  if (Wire.available())
+  {
+    retVal = Wire.read();
+  }
+  
+  return retVal;
+}
+
+uint8_t readFT5316TouchAddr( uint8_t regAddr, uint8_t * pBuf, uint8_t len )
+{
+  Wire.beginTransmission(addr);
+  Wire.write( regAddr );  // register 0
+  uint8_t retVal = Wire.endTransmission();
+  
+  uint8_t returned = Wire.requestFrom(addr, len);    // request 1 bytes from slave device #2
+  
+  uint8_t i;
+  for (i = 0; (i < len) && Wire.available(); i++)
+  {
+    pBuf[i] = Wire.read();
+  }
+  
+  return i;
+}
+
+uint8_t readFT5316TouchLocation( TouchLocation * pLoc, uint8_t num )
+{
+  uint8_t retVal = 0;
+  uint8_t i;
+  uint8_t k;
+  
+  do
+  {
+    if (!pLoc) break; // must have a buffer
+    if (!num)  break; // must be able to take at least one
+    
+    uint8_t status = readFT5316TouchRegister(2);
+    
+    static uint8_t tbuf[40];
+    
+    if ((status & 0x0f) == 0) break; // no points detected
+    
+    uint8_t hitPoints = status & 0x0f;
+    
+    Serial.print("number of hit points = ");
+    Serial.println( hitPoints );
+    
+    readFT5316TouchAddr( 0x03, tbuf, hitPoints*6);
+    
+    for (k=0,i = 0; (i < hitPoints*6)&&(k < num); k++, i += 6)
+    {
+      pLoc[k].x = (tbuf[i+0] & 0x0f) << 8 | tbuf[i+1];
+      pLoc[k].y = (tbuf[i+2] & 0x0f) << 8 | tbuf[i+3];
+    }
+    
+    retVal = k;
+    
+  } while (0);
+  
+  return retVal;
+}
+
+void writeFT5316TouchRegister( uint8_t reg, uint8_t val)
+{
+  Wire.beginTransmission(addr);
+  Wire.write( reg );  // register 0
+  Wire.write( val );  // value
+  
+  uint8_t retVal = Wire.endTransmission();  
+}
+
+void readOriginValues(void)
+{
+  writeFT5316TouchRegister(0, eTEST);
+  delay(1);
+  //uint8_t originLength = readFT5316TouchAddr(0x08, buf, 8 );
+  uint8_t originXH = readFT5316TouchRegister(0x08);
+  uint8_t originXL = readFT5316TouchRegister(0x09);
+  uint8_t originYH = readFT5316TouchRegister(0x0a);
+  uint8_t originYL = readFT5316TouchRegister(0x0b);
+  
+  uint8_t widthXH  = readFT5316TouchRegister(0x0c);
+  uint8_t widthXL  = readFT5316TouchRegister(0x0d);
+  uint8_t widthYH  = readFT5316TouchRegister(0x0e);
+  uint8_t widthYL  = readFT5316TouchRegister(0x0f);
+  
+  //if (originLength)
+  {
+    Serial.print("Origin X,Y = ");
+    Serial.print( uint16_t((originXH<<8) | originXL) );
+    Serial.print(", ");
+    Serial.println( uint16_t((originYH<<8) | originYL) );
+    
+    Serial.print("Width X,Y = ");
+    Serial.print( uint16_t((widthXH<<8) | widthXL) );
+    Serial.print(", ");
+    Serial.println( uint16_t((widthYH<<8) | widthYL) );
+  }
+  
+}
+
+
+uint32_t dist(const TouchLocation & loc)
+{
+  uint32_t retVal = 0;
+  
+  uint32_t x = loc.x;
+  uint32_t y = loc.y;
+  
+  retVal = x*x + y*y;
+  
+  return retVal;
+}
+
+uint32_t dist(const TouchLocation & loc1, const TouchLocation & loc2)
+{
+  uint32_t retVal = 0;
+  
+  uint32_t x = loc1.x - loc2.x;
+  uint32_t y = loc1.y - loc2.y;
+  
+  retVal = sqrt(x*x + y*y);
+  
+  return retVal;
+}
+
+bool sameLoc( const TouchLocation & loc, const TouchLocation & loc2 )
+{
+  return dist(loc,loc2) < 50;
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.setClock(400000);    
+  Wire.begin();        // join i2c bus (address optional for master)   
+  pinMode     (FT5316_INT, INPUT);//CTP INT INPUT
+  pinMode(FT5316_RST, OUTPUT);  //CTP RESET OUTPUT
+  digitalWrite(FT5316_RST, HIGH);//on
+  delay(10);
+  digitalWrite(FT5316_RST, LOW);//on
+  delay(10);
+  digitalWrite(FT5316_RST, HIGH);//on
+ 
+  
+  ER_TFT.Parallel_Init();
+  ER_TFT.HW_Reset();
+  ER_TFT.System_Check_Temp();
+  delay(100);
+  while(ER_TFT.LCD_StatusRead()&0x02);
+  ER_TFT.initial();
+  ER_TFT.Display_ON();
+
+}
+void loop() {
+   static uint16_t w = LCD_XSIZE_TFT;
+  static uint16_t h = LCD_YSIZE_TFT; 
+  uint8_t attention = digitalRead(FT5316_INT);
+   unsigned int i;
+   double float_data;  
+  
+  ER_TFT.Select_Main_Window_16bpp();
+  ER_TFT.Main_Image_Start_Address(layer1_start_addr);        
+  ER_TFT.Main_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.Main_Window_Start_XY(0,0);
+  ER_TFT.Canvas_Image_Start_address(0);
+  ER_TFT.Canvas_image_width(LCD_XSIZE_TFT);
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT); 
+  
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill(); 
+  
+   ER_TFT.Foreground_color_65k(White);
+  ER_TFT.Background_color_65k(Black);
+  ER_TFT.CGROM_Select_Internal_CGROM();  
+  ER_TFT.Font_Select_12x24_24x24();
+  ER_TFT.Goto_Text_XY(0,0); 
+  ER_TFT.Show_String( "www.buydisplay.com");   
+  ER_TFT.Goto_Text_XY(0,24); 
+  ER_TFT.Show_String( "Capacitive touch screen tese(5 point touch)"); 
+  ER_TFT.Goto_Text_XY(5,LCD_YSIZE_TFT-25); 
+  ER_TFT.Show_String( "Clear");       
+  
+  while(1) 
+  { attention = digitalRead(FT5316_INT);
+     if (!attention) 
+    {  
+      Serial.println("Touch: ");
+      
+      uint8_t count = readFT5316TouchLocation( touchLocations, 5 );
+                
+        for (i = 0; i < count; i++)
+        {
+            
+            if (((touchLocations[i].x)<50) &&((touchLocations[i].y)>LCD_YSIZE_TFT-30))
+              {    ER_TFT.Foreground_color_65k(Black);
+                  ER_TFT.Line_Start_XY(0,0);
+                  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+                  ER_TFT.Start_Square_Fill(); 
+                  
+                   ER_TFT.Foreground_color_65k(White);
+                  ER_TFT.Background_color_65k(Black);
+                  ER_TFT.CGROM_Select_Internal_CGROM();  
+                  ER_TFT.Font_Select_12x24_24x24();
+                  ER_TFT.Goto_Text_XY(0,0); 
+                  ER_TFT.Show_String( "www.buydisplay.com");   
+                  ER_TFT.Goto_Text_XY(0,24); 
+                  ER_TFT.Show_String( "Capacitive touch screen tese(5 point touch)");
+                  ER_TFT.Goto_Text_XY(5,LCD_YSIZE_TFT-25); 
+                  ER_TFT.Show_String( "Clear");                  
+              }  
+              
+          else{                                       
+              snprintf((char*)buf,sizeof(buf),"(%3d,%3d)",LCD_XSIZE_TFT-touchLocations[i].x,LCD_YSIZE_TFT-touchLocations[i].y); 
+            const  char *str=(const char *)buf;
+             ER_TFT.Foreground_color_65k(Red);  
+            ER_TFT.Text_Mode();
+            ER_TFT.Goto_Text_XY(50,80+24*i);
+            ER_TFT.LCD_CmdWrite(0x04);
+            while(*str != '\0')
+            {
+            ER_TFT.LCD_DataWrite(*str);
+            ER_TFT.Check_Mem_WR_FIFO_not_Full();      
+            ++str; 
+            } 
+            ER_TFT.Check_2D_Busy();
+            ER_TFT.Graphic_Mode(); //back to graphic mode;
+           
+            if(i==0)  ER_TFT.DrawCircle_Fill(touchLocations[i].x,touchLocations[i].y, 3, Red);  
+            else if(i==1)  ER_TFT.DrawCircle_Fill(touchLocations[i].x,touchLocations[i].y, 3, Green); 
+            else if(i==2)  ER_TFT.DrawCircle_Fill(touchLocations[i].x,touchLocations[i].y, 3, Blue);        
+            else if(i==3)  ER_TFT.DrawCircle_Fill(touchLocations[i].x,touchLocations[i].y, 3, Cyan); 
+             else if(i==4)  ER_TFT.DrawCircle_Fill(touchLocations[i].x,touchLocations[i].y, 3, Yellow);     
+          }
+        }
+     } 
+    
+  }
+ 
+}
+
+
+
---
+++ BuyDisplay/ESP32/Display_test/Display_test.ino
@@ -0,0 +1,472 @@
+/***************************************************
+//Web: http://www.buydisplay.com
+EastRising Technology Co.,LTD
+Examples for ER-PCBA6105RA8889+ER-TFT070-2 Display test
+Display is Hardward SPI 4-Wire SPI Interface and 5V Power Supply
+Tested and worked with:
+Works with Arduino 2.3.4 IDE  
+NOTE: test OK:ESP32-WROOM-32 
+****************************************************/
+
+/*
+  ==TFT Hardware SPI to ESP32  ==
+    TFT   =>    ESP32
+  *1,2. GND       ->    GND
+  *3,4. VCC       ->    5V    3.3V OR 5V is optional, depending on the voltage of the module purchased 
+  *5. CS        ->       5
+  *6. MISO        ->    19  
+  *7. MOSI       ->    23  
+  *8. SCK       ->    18  
+  *11. RES       ->    16   
+*/
+
+#include <SPI.h>
+#include <Wire.h>
+#include "RA8889.h"
+
+void setup() {
+  
+
+  ER_TFT.Parallel_Init();
+  ER_TFT.HW_Reset();
+  ER_TFT.System_Check_Temp();
+  delay(100);
+  while(ER_TFT.LCD_StatusRead()&0x02);
+  ER_TFT.initial();
+  ER_TFT.Display_ON();
+
+}
+void loop() {
+  ER_TFT.Select_Main_Window_16bpp();
+  ER_TFT.Main_Image_Start_Address(layer1_start_addr);        
+  ER_TFT.Main_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.Main_Window_Start_XY(0,0);
+  ER_TFT.Canvas_Image_Start_address(0);
+  ER_TFT.Canvas_image_width(LCD_XSIZE_TFT);
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT); 
+  
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Red);
+  delay(1000);
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Green);
+  delay(1000);
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Blue);
+  delay(1000);
+
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Cyan);
+  delay(1000);
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Yellow);
+  delay(1000); 
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Purple);
+  delay(1000);   
+ 
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,Black);
+  delay(1000); 
+  ER_TFT.DrawSquare_Fill(0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,White);
+  delay(1000);
+
+  ////////BackLight Brightness control test  whit ER's PWM0
+  unsigned char  brightness=10;
+  ER_TFT.Foreground_color_65k(White);
+  ER_TFT.Background_color_65k(Red);
+  ER_TFT.CGROM_Select_Internal_CGROM();  
+  ER_TFT.Font_Select_12x24_24x24();
+  ER_TFT.Goto_Text_XY(0,10); 
+  ER_TFT.Show_String( "BackLight Brightness control");
+  while(brightness<=100)
+ {
+  ER_TFT.Select_PWM1();
+  ER_TFT.Set_PWM_Prescaler_1_to_256(20);
+  ER_TFT.Select_PWM1_Clock_Divided_By_1();
+  ER_TFT.Set_Timer1_Count_Buffer(100); 
+  ER_TFT.Set_Timer1_Compare_Buffer(brightness); 
+  ER_TFT.Start_PWM1(); 
+  delay(50);
+  brightness+=10;
+  } 
+   delay(1000); 
+   
+  ////////Drawing
+  unsigned int i;
+  ER_TFT.Select_Main_Window_16bpp();
+  ER_TFT.Main_Image_Start_Address(layer1_start_addr);				
+  ER_TFT.Main_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.Main_Window_Start_XY(0,0);
+  ER_TFT.Canvas_Image_Start_address(layer1_start_addr);
+  ER_TFT.Canvas_image_width(LCD_XSIZE_TFT);
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT);	
+
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Red);
+  ER_TFT.Line_Start_XY(0+i,0+i);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Start_Square();
+  delay(10);
+  }
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0+i,0+i);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Start_Square();
+  delay(10);
+  }
+ delay(100);
+///////////////////////////Square Of Circle
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Green);
+  ER_TFT.Line_Start_XY(0+i,0+i);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Circle_Square_Radius_RxRy(10,10);
+  ER_TFT.Start_Circle_Square();
+  delay(10);
+  }
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0+i,0+i);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Circle_Square_Radius_RxRy(10,10);
+  ER_TFT.Start_Circle_Square();
+  delay(10);
+  }
+  delay(100);
+
+///////////////////////////Circle
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Blue);
+  ER_TFT.Circle_Center_XY(LCD_XSIZE_TFT/2,LCD_YSIZE_TFT/2);
+  ER_TFT.Circle_Radius_R(i);
+  ER_TFT.Start_Circle_or_Ellipse();
+  delay(10);
+  }
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Circle_Center_XY(LCD_XSIZE_TFT/2,LCD_YSIZE_TFT/2);
+  ER_TFT.Circle_Radius_R(i);
+  ER_TFT.Start_Circle_or_Ellipse();
+  delay(10);
+  }
+  delay(100);
+
+///////////////////////////Ellipse
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(White);
+  ER_TFT.Circle_Center_XY(LCD_XSIZE_TFT/2,LCD_YSIZE_TFT/2);
+  ER_TFT.Ellipse_Radius_RxRy(i+100,i);
+  ER_TFT.Start_Circle_or_Ellipse();
+  delay(10);
+  }
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Circle_Center_XY(LCD_XSIZE_TFT/2,LCD_YSIZE_TFT/2);
+  ER_TFT.Ellipse_Radius_RxRy(i+100,i);
+  ER_TFT.Start_Circle_or_Ellipse();
+  delay(10);
+  }
+  delay(100);
+
+ ////////////////////////////Triangle
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Yellow);
+  ER_TFT.Triangle_Point1_XY(LCD_XSIZE_TFT/2,i);
+  ER_TFT.Triangle_Point2_XY(i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Triangle_Point3_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Start_Triangle();
+  delay(10);
+  }
+
+  for(i=0;i<=LCD_YSIZE_TFT/2-10;i+=8)
+  {ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Triangle_Point1_XY(LCD_XSIZE_TFT/2,i);
+  ER_TFT.Triangle_Point2_XY(i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Triangle_Point3_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Start_Triangle();
+  delay(10);
+  }
+  delay(100);
+
+
+ ////////////////////////////line
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+
+  for(i=0;i<=LCD_XSIZE_TFT;i+=8)
+  {ER_TFT.Foreground_color_65k(Red);
+  ER_TFT.Line_Start_XY(i,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Line();
+  delay(10);
+  }
+  for(i=0;i<=LCD_YSIZE_TFT;i+=8)
+  {ER_TFT.Foreground_color_65k(Red);
+  ER_TFT.Line_Start_XY(0,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,i);
+  ER_TFT.Start_Line();
+  delay(10);
+  }
+
+
+  for(i=0;i<=LCD_XSIZE_TFT;i+=8)
+  {ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(i,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1-i,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Line();
+  delay(10);
+  }
+  for(i=0;i<=LCD_YSIZE_TFT;i+=8)
+  {ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,LCD_YSIZE_TFT-1-i);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,i);
+  ER_TFT.Start_Line();
+  delay(10);
+  }
+
+
+  delay(100);  
+
+/////////////Text  
+  ER_TFT.Select_Main_Window_16bpp();
+  ER_TFT.Main_Image_Start_Address(layer1_start_addr);				
+  ER_TFT.Main_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.Main_Window_Start_XY(0,0);
+  ER_TFT.Canvas_Image_Start_address(layer1_start_addr);
+  ER_TFT.Canvas_image_width(LCD_XSIZE_TFT);
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT);	
+ 
+  
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+
+
+
+  ER_TFT.Foreground_color_65k(Green);
+  ER_TFT.Font_Select_12x24_24x24();
+  ER_TFT.Goto_Text_XY(0,26);
+  ER_TFT.Show_String("buydisplay.com");
+
+ ER_TFT.Background_color_65k(Black); 
+  ER_TFT.Foreground_color_65k(Red);
+  ER_TFT.Font_Width_X4(); 
+  ER_TFT.Font_Height_X4();
+  ER_TFT.Goto_Text_XY(0,90);  
+  ER_TFT.Show_String("buydisplay.com");    
+  delay(2000); 
+  ER_TFT.Font_Width_X1(); 
+  ER_TFT.Font_Height_X1();
+  
+  
+  unsigned int temp;
+  unsigned long im=1;
+  ER_TFT.Select_Main_Window_16bpp();
+  ER_TFT.Main_Image_Start_Address(0);				
+  ER_TFT.Main_Image_Width(LCD_XSIZE_TFT);							
+  ER_TFT.Main_Window_Start_XY(0,0);
+
+  ER_TFT.Canvas_Image_Start_address(0);//Layer 1
+  ER_TFT.Canvas_image_width(LCD_XSIZE_TFT);//
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT);
+
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-25);
+  ER_TFT.Start_Square_Fill();
+
+  ER_TFT.Foreground_color_65k(Blue);
+  ER_TFT.Line_Start_XY(0,LCD_YSIZE_TFT-24);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+  ER_TFT.Foreground_color_65k(White);
+  ER_TFT.Background_color_65k(Blue);
+  ER_TFT.CGROM_Select_Internal_CGROM();
+  ER_TFT.Font_Select_12x24_24x24();
+  ER_TFT.Goto_Text_XY(0,LCD_YSIZE_TFT-24);
+  ER_TFT.Show_String("  Demo BTE Compare");
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Background_color_65k(White);
+ 
+  ER_TFT.Goto_Text_XY(0,LCD_YSIZE_TFT-48);
+  ER_TFT.Show_String("Execute Logic 'OR' 0xf000");
+
+  ER_TFT.Active_Window_XY(20,40);
+  ER_TFT.Active_Window_WH(80,80);
+  ER_TFT.Goto_Pixel_XY(20,40);
+  ER_TFT. Show_picture(80*80,pic_80x80); 
+  ER_TFT.Active_Window_XY(20+80+20,40);
+  ER_TFT.Active_Window_WH(80,80);
+  ER_TFT.Goto_Pixel_XY(120,40);
+  ER_TFT.Show_picture(80*80,pic_80x80);
+  ER_TFT.Active_Window_XY(20+80+20+80+20,40);
+  ER_TFT.Active_Window_WH(80,80);
+  ER_TFT.Goto_Pixel_XY(220,40);
+  ER_TFT.Show_picture(80*80,pic_80x80);		 
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT);
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Background_color_65k(White);
+  ER_TFT.CGROM_Select_Internal_CGROM();
+ 
+  ER_TFT.Goto_Text_XY(20,130 );
+  ER_TFT.Show_String("NO BTE");
+  ER_TFT.Goto_Text_XY(120,130 );
+  ER_TFT.Show_String("BTE ");
+  ER_TFT.Goto_Text_XY(120,154 );
+  ER_TFT.Show_String("ROP");
+  ER_TFT.Goto_Text_XY(220,130 );
+  ER_TFT.Show_String("BTE Move");
+  ER_TFT.Goto_Text_XY(220,154 );
+  ER_TFT.Show_String("ROP");
+  delay(1000);
+  ER_TFT.Active_Window_XY(20,40);
+  ER_TFT.Active_Window_WH(80,80); 
+  ER_TFT. Goto_Pixel_XY(20,40);
+  ER_TFT.LCD_CmdWrite(0x04);
+  temp =   ER_TFT.LCD_DataRead();
+  ER_TFT.Check_Mem_RD_FIFO_not_Empty();  //dummy
+  for(i=0; i<80*80;i++)
+  {				
+  temp =   ER_TFT.LCD_DataRead();		   
+  temp=temp|(  ER_TFT.LCD_DataRead()<<8);
+  ER_TFT.Check_Mem_RD_FIFO_not_Empty();
+  temp |= 0xf000; 
+  ER_TFT.LCD_DataWrite(temp);
+  ER_TFT.LCD_DataWrite(temp>>8);
+  ER_TFT.Check_Mem_WR_FIFO_not_Full();
+  }
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT);
+  delay(1000);
+   //second block, MCU write with BTE ROP 
+  ER_TFT.BTE_S0_Color_16bpp();
+  ER_TFT.BTE_S1_Color_16bpp();
+  ER_TFT.BTE_S1_Memory_Start_Address(0);
+  ER_TFT.BTE_S1_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.BTE_S1_Window_Start_XY(120,40);
+
+  ER_TFT.BTE_Destination_Color_16bpp();  
+  ER_TFT.BTE_Destination_Memory_Start_Address(0);
+  ER_TFT.BTE_Destination_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.BTE_Destination_Window_Start_XY(120,40);  
+  ER_TFT.BTE_Window_Size(80,80);
+
+  ER_TFT.BTE_ROP_Code(14);
+  ER_TFT.BTE_Operation_Code(0); //BTE write
+  ER_TFT.BTE_Enable();
+
+  ER_TFT.LCD_CmdWrite(0x04);
+  for(i=0; i<80*80;i++)
+  {				
+  ER_TFT.LCD_DataWrite(0xf000);
+  ER_TFT.LCD_DataWrite(0xf000>>8);
+  ER_TFT.Check_Mem_WR_FIFO_not_Full();
+  }
+  ER_TFT.Check_Mem_WR_FIFO_Empty();//糶Ч浪琩
+  ER_TFT.Check_BTE_Busy();
+
+  delay(1000);   
+	  //third block, BTE MOVE with ROP
+  ER_TFT.Canvas_Image_Start_address(layer2_start_addr);//
+  ER_TFT.Canvas_image_width(LCD_XSIZE_TFT);//
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT);
+
+ 
+  ER_TFT.Foreground_color_65k(0xf000);
+  ER_TFT.Background_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,40);
+  ER_TFT.Line_End_XY(80,120);
+  ER_TFT.Start_Square_Fill();  
+   
+   
+  ER_TFT.BTE_S0_Color_16bpp();
+  ER_TFT.BTE_S0_Memory_Start_Address(layer2_start_addr);
+  ER_TFT.BTE_S0_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.BTE_S0_Window_Start_XY(0,40);
+
+  ER_TFT.BTE_S1_Color_16bpp();
+  ER_TFT.BTE_S1_Memory_Start_Address(layer1_start_addr);
+  ER_TFT.BTE_S1_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.BTE_S1_Window_Start_XY(220,40);
+
+  ER_TFT.BTE_Destination_Color_16bpp();  
+  ER_TFT.BTE_Destination_Memory_Start_Address(layer1_start_addr);
+  ER_TFT.BTE_Destination_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.BTE_Destination_Window_Start_XY(220,40);  
+  ER_TFT.BTE_Window_Size(80,80);
+
+  ER_TFT.BTE_ROP_Code(14);
+  ER_TFT.BTE_Operation_Code(2); //BTE write
+  ER_TFT.BTE_Enable();
+  ER_TFT.Check_BTE_Busy();
+  delay(1000);  
+  ER_TFT.BTE_Disable();
+
+  
+ ///////////////////////// DMA
+  ER_TFT.Select_Main_Window_16bpp();
+  ER_TFT.Main_Image_Start_Address(layer1_start_addr);				
+  ER_TFT.Main_Image_Width(LCD_XSIZE_TFT);
+  ER_TFT.Main_Window_Start_XY(0,0);
+  ER_TFT.Canvas_Image_Start_address(layer1_start_addr);
+  ER_TFT.Canvas_image_width(LCD_XSIZE_TFT);
+  ER_TFT.Active_Window_XY(0,0);
+  ER_TFT.Active_Window_WH(LCD_XSIZE_TFT,LCD_YSIZE_TFT);	
+ 
+  
+  ER_TFT.Foreground_color_65k(Black);
+  ER_TFT.Line_Start_XY(0,0);
+  ER_TFT.Line_End_XY(LCD_XSIZE_TFT-1,LCD_YSIZE_TFT-1);
+  ER_TFT.Start_Square_Fill();
+ 
+ 
+  ER_TFT.DMA_24bit_Block(1,1,0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,LCD_XSIZE_TFT,0);
+// Select SPI : SCS��0       SCS��1
+// SPI Clock = System Clock /{(Clk+1)*2}
+// Transfer to SDRAM address:X1
+ // Transfer to SDRAM address:Y1
+// DMA data width
+// DMA data height
+// Picture's width
+// Flash address
+  delay(2000);
+  ER_TFT.DMA_24bit_Block(1,1,0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,LCD_XSIZE_TFT,768000);
+  delay(2000);  
+  ER_TFT.DMA_24bit_Block(1,1,0,0,LCD_XSIZE_TFT,LCD_YSIZE_TFT,LCD_XSIZE_TFT,1536000);
+  delay(2000); 
+  
+    
+}
